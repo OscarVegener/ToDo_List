969ff9ce3e21ce8998fafdc59d553a8527e6bf2b
OscarVegener <49143627+OscarVegener@users.noreply.github.com>
2020-10-08 19:29:37 +0300

    Completed stage 2

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-# This is a sample Python script.
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+Base = declarative_base()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Table(Base):
+    __tablename__ = 'task'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    task = Column(String, default='default_value')
+    deadline = Column(Date, default=datetime.today())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __repr__(self):
+        return self.task
+
+    def __str__(self):
+        return str(self.id) + ". " + self.task
+
+
+def add_task():
+    print("Enter task")
+    task = input()
+    new_row = Table(task=task)
+    session.add(new_row)
+    session.commit()
+
+
+def tasks():
+    print()
+    print("Today:")
+    rows = session.query(Table).all()
+    if len(rows) > 0:
+        for row in rows:
+            print(row)
+        print()
+    else:
+        print("Nothing to do!")
+        print()
+
+
+def delete_tasks():
+    pass
+
+
+def main_menu():
+    while True:
+        print("1) Today's tasks")
+        print("2) Add task")
+        print("0) Exit")
+        usr_input = int(input())
+        if usr_input == 0:
+            break
+        elif usr_input == 1:
+            tasks()
+        elif usr_input == 2:
+            add_task()
+
+
+engine = create_engine('sqlite:///todo.db?check_same_thread=False')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+main_menu()
